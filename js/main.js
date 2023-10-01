fa22d37ff5cd1ba6a00d9cfcee1130ce48b6e6af
Ridoyhasan00 <138436331+Ridoyhasan00@users.noreply.github.com>
2023-10-02 00:48:30 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//wow
+new WOW().init();
+
+/*
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:5000,
+        smartSpeed:1000,
+        autoplayHoverPause:true,
+
+        responsive:{
+            768:{
+                items:2,
+            }
+        }    
+    });
+}); */
+
+
